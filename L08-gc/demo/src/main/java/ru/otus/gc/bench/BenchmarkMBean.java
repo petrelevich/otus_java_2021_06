9412709f82681e0264b3eb9229772d15dd186c50
sergey <petrelevich@yandex.ru>
2021-07-25 23:29:08 +0300

    L08 gc

--- L08-gc/demo/src/main/java/ru/otus/gc/bench/BenchmarkMBean.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.gc.bench;
-
-public interface BenchmarkMBean {
-  int getSize();
-
-  void setSize(int size);
-}
--- L08-gc/demo/src/main/java/ru/otus/gc/bench/GcDemo.java
+++ L08-gc/demo/src/main/java/ru/otus/gc/bench/GcDemo.java
@@ -5,19 +5,16 @@ import com.sun.management.GarbageCollectionNotificationInfo;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.List;
-import javax.management.MBeanServer;
-import javax.management.NotificationEmitter;
-import javax.management.NotificationListener;
-import javax.management.ObjectName;
+import javax.management.*;
 import javax.management.openmbean.CompositeData;
 
 /*
 О формате логов
 http://openjdk.java.net/jeps/158
 
-
 -Xms512m
 -Xmx512m
+-verbose:gc
 -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
 -XX:+HeapDumpOnOutOfMemoryError
 -XX:HeapDumpPath=./logs/dump
@@ -25,48 +22,57 @@ http://openjdk.java.net/jeps/158
 */
 
 /*
-1)
-    default, time: 83 sec (82 without Label_1)
-2)
-    -XX:MaxGCPauseMillis=100000, time: 82 sec //Sets a target for the maximum GC pause time.
-3)
-    -XX:MaxGCPauseMillis=10, time: 91 sec
-
-4)
--Xms2048m
--Xmx2048m
-    time: 81 sec
-
-5)
--Xms5120m
--Xmx5120m
-    time: 80 sec
-
-5)
--Xms20480m
--Xmx20480m
-    time: 81 sec (72 without Label_1)
-
+основная настройка:
+    -XX:MaxGCPauseMillis=100000
+    -XX:MaxGCPauseMillis=10
 */
 
 public class GcDemo {
+    private volatile int objectArraySize = 5 * 1000 * 1000;
+
     public static void main(String... args) throws Exception {
         System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+
+        GcDemo gcDemo = new GcDemo();
+
         switchOnMonitoring();
+        switchOnMxBean(gcDemo);
+
+        gcDemo.run();
+    }
+
+    public int getObjectArraySize() {
+        return objectArraySize;
+    }
+
+    public void setObjectArraySize(int objectArraySize) {
+        this.objectArraySize = objectArraySize;
+    }
+
+    private void run() throws InterruptedException {
         long beginTime = System.currentTimeMillis();
-
-        int size = 5 * 1000 * 1000;
-        int loopCounter = 1000;
-        //int loopCounter = 100000;
-        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        ObjectName name = new ObjectName("ru.otus:type=Benchmark");
-
-        Benchmark mbean = new Benchmark(loopCounter);
-        mbs.registerMBean(mbean, name);
-        mbean.setSize(size);
-        mbean.run();
-
+        createObject();
         System.out.println("time:" + (System.currentTimeMillis() - beginTime) / 1000);
+    }
+
+    private void createObject() throws InterruptedException {
+        int loopCounter = 1000;
+        for (int loopIdx = 0; loopIdx < loopCounter; loopIdx++) {
+            int local = objectArraySize;
+            Object[] array = new Object[local];
+            for (int idx = 0; idx < local; idx++) {
+                array[idx] = new String(new char[0]);
+            }
+            Thread.sleep(10); //Label_1
+        }
+    }
+
+    private static void switchOnMxBean(GcDemo gcDemo) throws MalformedObjectNameException, NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException, InterruptedException {
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("ru.otus:type=gcDemo");
+
+        GcDemoControlMBean mbean = new GcDemoControl(gcDemo);
+        mbs.registerMBean(mbean, name);
     }
 
     private static void switchOnMonitoring() {
