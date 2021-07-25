4103c9e426c2ed4758667274d393d9a9da50b5f3
sergey <petrelevich@yandex.ru>
2021-07-25 21:06:26 +0300

    L08 gc

---
+++ L08-gc/homework/src/main/java/ru/calculator/Summator.java
@@ -0,0 +1,53 @@
+package ru.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Summator {
+    private Integer sum = 0;
+    private Integer prevValue = 0;
+    private Integer prevPrevValue = 0;
+    private Integer sumLastThreeValues = 0;
+    private Integer someValue = 0;
+    private final List<Data> logValues = new ArrayList<>();
+
+    public void calc(Data data) {
+        if (data.getValue() % 15 == 0) {
+            logValues.add(data);
+        }
+        if (logValues.size() == 60_000_000) {
+            logValues.clear();
+        }
+        sum += data.getValue();
+
+        sumLastThreeValues = data.getValue() + prevValue + prevPrevValue;
+
+        prevPrevValue = prevValue;
+        prevValue = data.getValue();
+
+        for (var idx = 0; idx < 3; idx++) {
+            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
+            someValue = Math.abs(someValue) + logValues.size();
+        }
+    }
+
+    public Integer getSum() {
+        return sum;
+    }
+
+    public Integer getPrevValue() {
+        return prevValue;
+    }
+
+    public Integer getPrevPrevValue() {
+        return prevPrevValue;
+    }
+
+    public Integer getSumLastThreeValues() {
+        return sumLastThreeValues;
+    }
+
+    public Integer getSomeValue() {
+        return someValue;
+    }
+}
